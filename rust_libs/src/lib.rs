f87e069b82e7582bbc2e193b789c1d90699dab5c
Namiki Shimoo <naminori4mo@gmail.com>
2021-12-26 22:42:47 +0900

    return x0(>=0) in extgcd

---
+++ rust_libs/src/lib.rs
@@ -0,0 +1,2 @@
+pub mod math_extgcd;
+pub use math_extgcd::ext_gcd;
--- rust_libs/src/math_extgcd.rs
+++ rust_libs/src/math_extgcd.rs
@@ -23,11 +23,13 @@ fn _ext_gcd(a: i64, b: i64) -> (i64, i64) {
 
 /*
     ax + by = c -> (x0, y0, xd, yd)
-    x = x0 + xd*t
-    y = y0 + yd*t
+    @ x = x0 + xd*t
+    @ y = y0 + yd*t
+    @ x0 is minimum non-negative integer
+      (or "x0 < 0" && "xd == 0"  (e.g.) 3x = -15)
     NOTICE: Do not input (a,b,c) = (0,0,0)
 */
-fn ext_gcd(a: i64, b: i64, c: i64) -> Option<(i64, i64, i64, i64)> {
+pub fn ext_gcd(a: i64, b: i64, c: i64) -> Option<(i64, i64, i64, i64)> {
     if a == 0 && b == 0 {
         if c == 0 {
             return Some((0, 0, 0, 0)); // exception ( any (x,y) is OK )
@@ -42,15 +44,47 @@ fn ext_gcd(a: i64, b: i64, c: i64) -> Option<(i64, i64, i64, i64)> {
     let (mut x, mut y) = _ext_gcd(a, b);
     x *= c;
     y *= c;
-    if a * x + b * y == c * (-1) {
+    if a as i128 * x as i128 + b as i128 * y as i128 == c as i128 * (-1) {
         x *= -1;
         y *= -1;
     }
-    Some((x, y, b, -a))
+    let (mut xd, mut yd) = (b, -a);
+    if xd == 0 {
+        return Some((x, y, xd, yd));
+    }
+    if xd < 0 {
+        xd *= -1;
+        yd *= -1;
+    }
+    /* calc min x (>= 0) */
+    if x >= 0 {
+        let minus_cnt = x / xd;
+        x -= minus_cnt * xd;
+        y -= minus_cnt * yd;
+    } else if x < 0 {
+        let plus_cnt = (-x - 1) / xd + 1;
+        x += plus_cnt * xd;
+        y += plus_cnt * yd;
+    }
+    Some((x, y, xd, yd))
 }
 
+// https://atcoder.jp/contests/abc186/tasks/abc186_e
 // #[fastout]
-fn main() {}
+fn main() {
+    input! {t: usize}
+    for _ in 0..t {
+        input! {n: i64, s: i64, k: i64}
+        let v = ext_gcd(k, -n, -s);
+        match v {
+            None => println!("-1"),
+            Some(v) => {
+                let (x, _y, _xd, _yd) = v;
+                println!("{}", x);
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -76,6 +110,9 @@ mod tests {
                         }
                         Some(v) => {
                             let (x, y, xd, yd) = v;
+                            /* check if x is minimum non-negative integer (or xd==0)*/
+                            assert!(x >= 0 || xd == 0);
+                            assert!(x - xd < 0 || xd == 0);
                             for t in -3..3 {
                                 let val = a * (x + xd * t) + b * (y + yd * t);
                                 assert!(val == c, "{:?} -> {:?}", (a, b, c), v);
